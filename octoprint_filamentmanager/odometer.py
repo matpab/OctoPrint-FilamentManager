4955ce04fdcc426d33edc1520d7c8cf7b2f3a84f
Matthias Pabel <matthias.pabel@hs-augsburg.de>
2020-04-15 10:59:32 +0200

    add support for M200

--- octoprint_filamentmanager/odometer.py
+++ octoprint_filamentmanager/odometer.py
@@ -5,13 +5,15 @@ __author__ = "Sven Lohrmann <malnvenshorn@gmail.com> based on work by Gina H√§u√
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = "Copyright (C) 2017 Sven Lohrmann - Released under terms of the AGPLv3 License"
 
+import math
 import re
 
 
 class FilamentOdometer(object):
 
     regexE = re.compile(r'.*E(-?\d+(\.\d+)?)')
-    regexT = re.compile(r'^T(\d+)')
+    regexT = re.compile(r'[tT](\d+)')
+    regexD = re.compile(r'[dD](\d+(\.\d+)?)')
 
     def __init__(self):
         self.g90_extruder = True
@@ -24,6 +26,9 @@ class FilamentOdometer(object):
         self.totalExtrusion = [0.0]
         self.maxExtrusion = [0.0]
         self.currentTool = 0
+        self.volumetricExtrusion = [False]
+        self.filamentDiametersByM200 = [1.75]
+        self._baseAreas = [self._calc_base_area(1.75)]  # save base areas for less computations while printing
 
     def reset_extruded_length(self):
         tools = len(self.maxExtrusion)
@@ -37,6 +42,10 @@ class FilamentOdometer(object):
         if gcode == "G1" or gcode == "G0":  # move
             e = self._get_float(cmd, self.regexE)
             if e is not None:
+                if self.volumetricExtrusion[self.currentTool]:
+                    # if volumetric extrusion is active for current tool convert volume to mm
+                    e = e / self._baseAreas[self.currentTool]
+
                 if self.relativeMode or self.relativeExtrusion:
                     # e is already relative, nothing to do
                     pass
@@ -71,6 +80,23 @@ class FilamentOdometer(object):
                         self.lastExtrusion.append(0.0)
                         self.totalExtrusion.append(0.0)
                         self.maxExtrusion.append(0.0)
+        elif gcode.startswith("M200"):  # volumetric extrusion
+            t = self._get_int(cmd, self.regexT, "search")
+            if t is not None:
+                if len(self.volumetricExtrusion) <= t:
+                    for i in xrange(len(self.volumetricExtrusion), t + 1):
+                        self.volumetricExtrusion.append(False)
+                        self.filamentDiametersByM200.append(1.75)
+                        self._baseAreas.append(self._calc_base_area(1.75))
+            else:
+                t = self.currentTool
+            d = self._get_float(cmd, self.regexD, "search")
+            if d is not None and d > 0:
+                self.volumetricExtrusion[t] = True
+                self.filamentDiametersByM200[t] = d
+                self._baseAreas[t] = self._calc_base_area(d)
+            else:
+                self.volumetricExtrusion[t] = False
 
     def set_g90_extruder(self, flag=True):
         self.g90_extruder = flag
@@ -81,16 +107,19 @@ class FilamentOdometer(object):
     def get_current_tool(self):
         return self.currentTool
 
-    def _get_int(self, cmd, regex):
-        result = regex.match(cmd)
+    def _get_int(self, cmd, regex, match_type="match"):
+        result = regex.search(cmd) if match_type == "search" else regex.match(cmd)
         if result is not None:
             return int(result.group(1))
         else:
             return None
 
-    def _get_float(self, cmd, regex):
-        result = regex.match(cmd)
+    def _get_float(self, cmd, regex, match_type="match"):
+        result = regex.search(cmd) if match_type == "search" else regex.match(cmd)
         if result is not None:
             return float(result.group(1))
         else:
             return None
+
+    def _calc_base_area(self, d):
+        return math.pi * (d/2)**2
